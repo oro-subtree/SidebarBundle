794e1548a97dbd6df1dc0488741be561e0e0913a
yurio <yurio@ua.fm>
2014-02-26 20:58:12 +0200

    BAP-3164: Update migrations, fix installer

--- Migrations/Schema/v1_0/OroSidebarBundle.php
+++ Migrations/Schema/v1_0/OroSidebarBundle.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Oro\Bundle\SidebarBundle\Migrations\Schemas\v1_0;
+namespace Oro\Bundle\SidebarBundle\Migrations\Schema\v1_0;
 
 use Doctrine\DBAL\Schema\Schema;
 use Oro\Bundle\InstallerBundle\Migrations\Migration;
